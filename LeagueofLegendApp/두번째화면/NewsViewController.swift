b00fdcfaafacf2aa8318ff4fb526fc532f540670
parkhohyun <haor3@hanmail.net>
2022-11-03 20:58:41 +0900

    NewsViewController 에 Date를 이용하여 NewsViewDetailTableViewCell numberof / cellfor 구현

--- LeagueofLegendApp/두번째화면/NewsViewController.swift
+++ LeagueofLegendApp/두번째화면/NewsViewController.swift
@@ -8,15 +8,39 @@
 import UIKit
 
 class NewsViewController: UIViewController {
-
+    
+    var weekNumber: [String] = []
+    
+    let tableView: UITableView = {
+        let tableview = UITableView()
+        tableview.backgroundColor = .white
+        return tableview
+    }()
+    
+    let date = Date()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setup()
+        dateWeek()
         view.backgroundColor = .systemBackground
         // Do any additional setup after loading the view.
     }
     
 
+    
+    func setup() {
+        view.backgroundColor = .systemBackground
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(NewsViewDetailTableViewCell.self, forCellReuseIdentifier: "NewsViewDetailTableViewCell")
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints { make in
+            make.top.left.right.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
     /*
     // MARK: - Navigation
 
@@ -26,5 +50,55 @@ class NewsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    func dateWeek(){
+        if Calendar.current.component(.day, from: date) / 7 == 0 {
+            weekNumber.append("1")
+        } else if Calendar.current.component(.day, from: date) / 7 == 1 {
+            weekNumber.append("1")
+            weekNumber.append("2")
+        } else if Calendar.current.component(.day, from: date) / 7 == 2 {
+            weekNumber.append("1")
+            weekNumber.append("2")
+            weekNumber.append("3")
+        } else if Calendar.current.component(.day, from: date) / 7 == 3 {
+            weekNumber.append("1")
+            weekNumber.append("2")
+            weekNumber.append("3")
+            weekNumber.append("4")
+        } else {
+            weekNumber.append("1")
+            weekNumber.append("2")
+            weekNumber.append("3")
+            weekNumber.append("4")
+            weekNumber.append("5")
+        }
+    }
 
+}
+
+extension NewsViewController: UITableViewDelegate {
+    
+}
+
+extension NewsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       
+        let month = Calendar.current.component(.month, from: date)
+        return month / 7
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "NewsViewDetailTableViewCell") as? NewsViewDetailTableViewCell else { return UITableViewCell() }
+        cell.backgroundColor = .clear
+        cell.selectionStyle = .none
+        
+        let cellInfo = weekNumber[indexPath.row]
+        cell.setModel(model: cellInfo)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 300
+    }
 }
---
+++ LeagueofLegendApp/두번째화면/NewsViewDetailTableViewCell.swift
@@ -0,0 +1,65 @@
+//
+//  NewsViewDetailTableViewCell.swift
+//  LeagueofLegendApp
+//
+//  Created by 박호현 on 2022/11/03.
+//
+
+import UIKit
+
+class NewsViewDetailTableViewCell: UITableViewCell {
+    
+    let date = Date()
+    
+    let dateLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = .boldSystemFont(ofSize: 12)
+        label.textColor = .systemBackground
+        return label
+    }()
+    
+    let championrotationLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = .boldSystemFont(ofSize: 20)
+        label.textColor = .systemBackground
+        return label
+    }()
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        addView()
+        layoutSetting()
+        // Configure the view for the selected state
+    }
+    
+    func addView() {
+        addSubview(dateLabel)
+        addSubview(championrotationLabel)
+    }
+    
+    func layoutSetting() {
+        dateLabel.snp.makeConstraints { make in
+            make.top.equalTo(contentView.snp.top).offset(20)
+            make.left.equalTo(contentView.snp.left).offset(20)
+        }
+        
+        championrotationLabel.snp.makeConstraints { make in
+            make.top.equalTo(dateLabel.snp.bottom).offset(20)
+            make.left.equalTo(contentView.snp.left).offset(20)
+        }
+    }
+    
+    func setModel(model: String){
+        
+        dateLabel.text = "\(Calendar.current.component(.month, from: date))월 \(model)주"
+        championrotationLabel.text = "챔피언 로테이션"
+    }
+}
